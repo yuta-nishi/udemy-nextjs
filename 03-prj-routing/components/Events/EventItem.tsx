cf468a6ed91b1c978f22dafa8414b77ff7edbc07
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-06 23:56:40 +0900

    79. Adding Buttons & Icons

--- 03-prj-routing/components/Events/EventItem.tsx
+++ 03-prj-routing/components/Events/EventItem.tsx
@@ -1,6 +1,8 @@
-import Link from 'next/link';
-
 import { Event } from '@/types/event';
+import { AddressIcon } from '../Icons/AddressIcon';
+import { ArrowRightIcon } from '../Icons/ArrowRightIcon';
+import { DateIcon } from '../Icons/DateIcon';
+import { Button } from '../UI/Button';
 import classes from './EventItem.module.css';
 
 interface Props extends Event {}
@@ -21,14 +23,21 @@ export const EventItem: React.FC<Props> = ({ id, title, location, date, image })
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
---
+++ 03-prj-routing/components/UI/Button.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+import classes from './Button.module.css';
+
+interface Props {
+  link: string | null;
+  children: React.ReactNode;
+}
+
+export const Button: React.FC<Props> = ({ link, children }) => {
+  if (link) {
+    return (
+      <Link href={link} className={classes.btn}>
+        {children}
+      </Link>
+    );
+  }
+
+  return <button className={classes.btn}>{children}</button>;
+};
